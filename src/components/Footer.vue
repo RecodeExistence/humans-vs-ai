c3c90df4019cc76ec94494ecf0b2cdf661cd1aa6
Karl M <karl@fwdmotion.studio>
2019-10-05 16:33:47 -0500

    added a paypal donate button

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,13 @@
 <template>
-    <div>
-        <p class="text-blue-400 mt-8 mb-8 text-center">Submit bugs / features to our <a style="color:#97C475;" href="https://github.com/Humans-V-AI/humans-vs-ai/issues" target="_blank">Github</a>.<br> Join our <a style="color:#97C475;" href="https://discord.gg/nawvXTy" target="_blank">Discord</a>!</p>
+    <div class="flex mt-16 mb-8 px-32 justify-between">
+        <p class="text-blue-400 text-center">Submit bugs / features or contribute to our open source project on <a style="color:#97C475;" href="https://github.com/Humans-V-AI/humans-vs-ai/issues" target="_blank">Github</a>.<br> Join our <a style="color:#97C475;" href="https://discord.gg/nawvXTy" target="_blank">Discord</a>!</p>
+
+        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_s-xclick" />
+            <input type="hidden" name="hosted_button_id" value="HMWX2LK3HNVM6" />
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+            <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+        </form>
+
     </div>
 </template>
