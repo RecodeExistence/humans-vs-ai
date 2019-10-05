ef03e3a7f6877f687f5ae668e751a3d466e340ac
Karl M <karl@fwdmotion.studio>
2019-10-05 17:03:17 -0500

    fixed an issue where git wasn't serving the files

---
+++ dist/styles/tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  theme: {
+<<<<<<< HEAD
+    maxHeight: {
+      '0': '0',
+      '1/4': '25%',
+      '1/2': '50%',
+      '3/4': '75%',
+      'full': '100%',
+    },
+=======
+>>>>>>> staging
+    extend: {
+      width: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+    }
+  },
+  variants: {},
+  plugins: []
+  }
+}
