9448e3b19c4e3dfe7bc903d153dbb83cdb2f60c3
Karl M <karl@fwdmotion.studio>
2019-10-05 04:58:49 -0500

    fixed counters

--- src/components/Counters.vue
+++ src/components/Counters.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-wrap justify-center">
-      <p class="text-yellow-400 mx-2 text-2xl text-center mt-12"> Gold :  {{ this.gold }}</p>
-      <p class="text-red-400 mx-2 text-2xl text-center mt-12"> Damage :  {{ this.damage }}</p>
+      <p class="text-yellow-400 mx-2 text-2xl text-center mt-12"> Gold :  {{ gold }}</p>
+      <p class="text-red-400 mx-2 text-2xl text-center mt-12"> Damage :  {{ damage }}</p>
     </div>
 </template>
 
