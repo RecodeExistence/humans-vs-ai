67181f0c0139cda4ff405308353b23bbf491167a
Karl Murray <karl@karlmurray.dev>
2019-10-07 15:29:04 -0500

    Made the log colors a little darker

--- src/components/Log.vue
+++ src/components/Log.vue
@@ -3,7 +3,7 @@
         <div class="text-center h-64 overflow-y-scroll max-h-1/2 mt-6 border-solid border-2 border-yellow-400 p-4 w-full rounded">
             <p class="text-2xl text-center text-yellow-400 mb-3">Log</p>
             <ul class="mb-4">
-                <li v-for="(turn, index) in turns" :key="index" :class="{'bg-blue-400 mt-2 p-2 rounded text-white' :turn.isPlayer,'bg-red-400 mt-2 p-2 rounded text-white' :!turn.isPlayer}">
+                <li v-for="(turn, index) in turns" :key="index" :class="{'bg-blue-500 mt-2 p-2 rounded text-white' :turn.isPlayer,'bg-red-500 mt-2 p-2 rounded text-white' :!turn.isPlayer}">
                     {{ turn.text }}
                 </li>
             </ul>
