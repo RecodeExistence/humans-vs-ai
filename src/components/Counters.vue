83ef8468f03af5b00f3aa95a2a47a5db7c7044b0
Brittney Postma <brittney.postma@hotmail.com>
2019-10-17 16:53:36 -0400

    fixed Human damage

--- src/components/Counters.vue
+++ src/components/Counters.vue
@@ -4,12 +4,12 @@
       {{ currency }} :
       {{ gold }}
     </p>
-    <p class="text-red-400 mx-2 text-2xl text-center mt-12" v-if="started">
-      Human Damage :
-      {{ damage }}
+    <p class="text-red-400 mx-10 text-2xl text-center mt-12" v-if="started">
+      Human Attack Damage :
+      {{ playerDamage }}
     </p>
-    <p class="text-red-400 mx-2 text-2xl text-center mt-12" v-if="started">
-      AI Damage :
+    <p class="text-red-400 mx-10 text-2xl text-center mt-12" v-if="started">
+      AI Attack Damage :
       {{ damage }}
     </p>
   </div>
@@ -29,6 +29,9 @@ export default {
     },
     currency() {
       return this.$store.state.currency;
+    },
+    playerDamage() {
+      return this.$store.state.playerDamage;
     }
   }
 };
