551fd540757ff3c91506431f5ff928568cbb957d
Karl M <karl@karlmurray.dev>
2019-11-02 10:14:53 -0500

    Finished adding auth on the backend

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
+Route::middleware('auth:api')->get('/user', function(Request $request){
+    return $request->users();
+});
+
 Route::get('/', function () {
     return view('index');
 });
